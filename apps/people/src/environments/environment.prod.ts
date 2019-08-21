2f83e3c30d6b2603b9987952bda9dda07714fccc
Wojciech Janaszek <trash.wjanaszek@gmail.com>
2019-08-21 19:29:58 +0200

    feat(people-app): environment values

--- apps/people/src/environments/environment.prod.ts
+++ apps/people/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://my.api.mockaroo.com',
+  apiKeyValue: '5fe8cd60',
+  apiKeyName: 'X-API-Key',
+  cacheTime: 180 // 3 * 60 seconds
 };
--- apps/people/src/environments/environment.ts
+++ apps/people/src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://my.api.mockaroo.com',
+  apiKeyValue: '5fe8cd60',
+  apiKeyName: 'X-API-Key',
+  cacheTime: 180 // 3 * 60 seconds
 };
 
 /*
