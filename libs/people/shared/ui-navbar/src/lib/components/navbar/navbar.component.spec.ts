65e169b1c20d06c9df206b48c1f233c61f7a60e2
Wojciech Janaszek <trash.wjanaszek@gmail.com>
2019-08-22 23:12:26 +0200

    test(people-app): unit tests for navbar component

--- libs/people/shared/ui-navbar/src/lib/components/navbar/navbar.component.spec.ts
+++ libs/people/shared/ui-navbar/src/lib/components/navbar/navbar.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatToolbarModule } from '@angular/material';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -8,6 +10,7 @@ describe('NavbarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [MatToolbarModule, RouterTestingModule],
       declarations: [NavbarComponent]
     }).compileComponents();
   }));
@@ -20,5 +23,19 @@ describe('NavbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain mat-toolbar', () => {
+    const navbar: HTMLElement = fixture.nativeElement;
+    const matToolbar = navbar.querySelector('mat-toolbar');
+    expect(matToolbar).toBeDefined();
+  });
+
+  it('should contain router links to person and specification routes', () => {
+    const navbar: HTMLElement = fixture.nativeElement;
+    const routerLinkCollection = navbar.querySelectorAll('span');
+    expect(routerLinkCollection.length).toBe(2);
+    expect(routerLinkCollection[0].textContent).toBe('People app');
+    expect(routerLinkCollection[1].textContent).toBe('Specification');
   });
 });
