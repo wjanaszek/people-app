6e500aa8640eccdb1c074ff4017e1cb70e3b031c
Wojciech Janaszek <trash.wjanaszek@gmail.com>
2019-08-24 20:48:27 +0200

    fix(people-app): fix tests for app component

--- apps/people/karma.conf.js
+++ apps/people/karma.conf.js
@@ -1,25 +1,16 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-const { constants } = require('karma');
 
-module.exports = () => {
-  return {
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    port: 9876,
-    colors: true,
-    logLevel: constants.LOG_INFO,
-    autoWatch: true,
-    browserNoActivityTimeout: 60000,
-    browsers: ['ChromeHeadless'],
-    singleRun: true
-  };
+const { join } = require('path');
+const getBaseKarmaConfig = require('../../karma.conf');
+
+module.exports = function(config) {
+  const baseConfig = getBaseKarmaConfig();
+  config.set({
+    ...baseConfig,
+    coverageIstanbulReporter: {
+      ...baseConfig.coverageIstanbulReporter,
+      dir: join(__dirname, '../../coverage/apps/people')
+    }
+  });
 };
